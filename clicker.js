954bc1e02bd3af111919f72591452cbb7a721a29
Adam Johnson <42796667+YoursTrulyArianator@users.noreply.github.com>
2018-08-29 16:59:36 +1000

    Add files via upload

---
+++ clicker.js
@@ -0,0 +1,29 @@
+var click = 0;
+
+function increment(e) {
+	console.log(1);
+	var x = e.clientX - 240;
+	var y = e.clientY - 160;
+	var dist = Math.sqrt(y*y + x*x);
+	if (dist<50){
+		click++;
+		redraw();
+	}
+
+}
+var c = document.getElementById("Canvas");
+c.addEventListener("click", increment);
+var ctx = c.getContext("2d");
+
+function redraw() {
+	console.log(c);
+	console.log(ctx);
+	ctx.clearRect(0,0,c.width,c.height);
+	ctx.font="20px Verdana";
+	ctx.fillText("Clicks: " + click,190,20);
+	ctx.beginPath();
+	ctxfillStyle="red";
+	ctx.arc(c.width/2, c.height/2, 50,0,2*3.14);
+	ctx.fill();
+}
+redraw();
